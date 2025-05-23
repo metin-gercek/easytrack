17c49f618724010e182f479b31194f0825e9b71c
metin-gercek <metingercek@gmail.com>
2025-05-23 12:23:25 +0300

    add new task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
